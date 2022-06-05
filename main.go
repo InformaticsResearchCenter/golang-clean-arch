dfb5605e8689e13883f9540f3bd4fb8ac3ac5ef5
Tri Angga Dio Simamora <trianggadio@gmail.com>
2022-06-05 12:00:24 +0700

    deploy dev

---
+++ main.go
@@ -0,0 +1,27 @@
+package main
+
+import (
+	"fmt"
+	"github.com/julienschmidt/httprouter"
+	"iteung-api/helper"
+	"net/http"
+	"os"
+)
+
+func WeOnEnv(w http.ResponseWriter, r *http.Request, _ httprouter.Params) {
+	fmt.Fprintf(w, "we are on %s env", os.Getenv("APP_ENV"))
+}
+
+func main() {
+	router := httprouter.New()
+
+	router.GET("/api/v1/", WeOnEnv)
+
+	server := http.Server{
+		Addr:    "localhost:8080",
+		Handler: router,
+	}
+
+	err := server.ListenAndServe()
+	helper.PanicIfError(err)
+}
